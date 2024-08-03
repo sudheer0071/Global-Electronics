80f0d1da5ddd4efe4f6213ae6020466ecd79363c
sudheer0071 <sudheer1614@gmail.com>
2024-08-04 04:44:00 +0530

    recheck

--- frontend/app/components/home/Companies.tsx
+++ frontend/app/components/home/Companies.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link"
 import { CompaniesCard } from "../cards/companiesCard"
 
 export const Companies = ()=>{
-  const companies = ["Pro-face","Mitsubishi", "Omron", "Fanux", "Delta", "HITECH", "Yashkawa", "Keyence", "Sick", "Panacsonic", "intek/Weinvi", "Schneider", "Siemens" ,"Hongfa"] 
+  const companies = ["pro-face", "mitsubishi", "omron", "fanux", "delta", "hITECH", "sashkawa", "seyence", "sick", "sanacsonic", "intek/seinvi", "schneider", "siemens", "songfa"]
   return <div className=" py-32  bg-white text-black"> 
     <div className=" text-4xl font-semibold  flex justify-center">
     A Partial List of Manufacturers
@@ -12,7 +13,17 @@ export const Companies = ()=>{
     </div>
     </div>
     <div className=" grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 lg:grid-cols-5 mt-16 transition-all">
-      {companies.map((company, idx)=><CompaniesCard key={idx} num={idx} company={company} />)}
+      {companies.map((company, idx)=>
+      {   const formattedCompany = company.charAt(0).toUpperCase() + company.slice(1).toLowerCase();
+        return (
+      <Link key={idx} href={`/companies/${company}`}>
+        
+      <CompaniesCard key={idx} num={idx} company={formattedCompany} />
+      </Link>
+        )
+
+}
+      )}
      
     </div>
   </div>
